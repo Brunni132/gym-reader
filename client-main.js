5644c1735fce0ad4560c2a88bc36a60f0cdfb04c
Florian Florian <florian@be-spoke.io>
2020-02-21 12:08:26 +0900

    Re-indent and tables for attenuation, release, decay, sustain and attack rates and levels

--- client-main.js
+++ client-main.js
@@ -1,16 +1,16 @@
 import {SAMPLE_RATE, YM2612} from "./ym2612";
 
 Number.prototype.toHex = function(positions = 2) {
-  return this.toString(16).padStart(positions, '0');
+	return this.toString(16).padStart(positions, '0');
 };
 
 Number.prototype.toBin = function(positions = 8) {
-  return this.toString(2).padStart(positions, '0');
+	return this.toString(2).padStart(positions, '0');
 };
 
 async function run() {
-  const res = await window.fetch('file.gym');
-  if (!res.ok) throw Error('Unable to fetch gym file');
+	const res = await window.fetch('file.gym');
+	if (!res.ok) throw Error('Unable to fetch gym file');
 
 	const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 	const audioBuffer = audioCtx.createBuffer(1, 30 * SAMPLE_RATE, SAMPLE_RATE);
@@ -18,36 +18,36 @@ async function run() {
 	const samplesPerFrame = SAMPLE_RATE / 60;
 	let processedSamples = 0;
 
-  const ym = new YM2612();
-  let processedFrames = 0;
-  let buffer = new Uint8Array(await res.arrayBuffer());
-  buffer = buffer.subarray(428);
+	const ym = new YM2612();
+	let processedFrames = 0;
+	let buffer = new Uint8Array(await res.arrayBuffer());
+	buffer = buffer.subarray(428);
 
-  while (buffer.length > 0 && processedSamples < samples.length) {
-    switch (buffer[0]) {
-    case 0:
-    	ym.processFrame();
-    	processedFrames += 1;
-      buffer = buffer.subarray(1);
-			ym.processSamples(samples.subarray(processedSamples, Math.min(processedSamples + samplesPerFrame, samples.length)));
-			processedSamples += samplesPerFrame;
-      break;
-    case 1:
-    	ym.processWrite(0, buffer[1], buffer[2]);
-      buffer = buffer.subarray(3);
-      break;
-    case 2:
-			ym.processWrite(1, buffer[1], buffer[2]);
-      buffer = buffer.subarray(3);
-      break;
-    case 3:
-      console.warn(`PSG data=${buffer[1].toHex()}`);
-      buffer = buffer.subarray(2);
-      break;
-    default:
-      throw new Error(`Unknown command ${buffer[0]}`);
-    }
-  }
+	while (buffer.length > 0 && processedSamples < samples.length) {
+		switch (buffer[0]) {
+			case 0:
+				ym.processFrame();
+				processedFrames += 1;
+				buffer = buffer.subarray(1);
+				ym.processSamples(samples.subarray(processedSamples, Math.min(processedSamples + samplesPerFrame, samples.length)));
+				processedSamples += samplesPerFrame;
+				break;
+			case 1:
+				ym.processWrite(0, buffer[1], buffer[2]);
+				buffer = buffer.subarray(3);
+				break;
+			case 2:
+				ym.processWrite(1, buffer[1], buffer[2]);
+				buffer = buffer.subarray(3);
+				break;
+			case 3:
+				console.warn(`PSG data=${buffer[1].toHex()}`);
+				buffer = buffer.subarray(2);
+				break;
+			default:
+				throw new Error(`Unknown command ${buffer[0]}`);
+		}
+	}
 
 	const source = audioCtx.createBufferSource();
 	source.buffer = audioBuffer;
--- ym2612.js
+++ ym2612.js
@@ -1,5 +1,29 @@
 export const SAMPLE_RATE = 48000;
 export const FM_OVER_144 = 7670454 / 144; // Japan Mega Drive
+const PHASE_ATTACK = 0, PHASE_DECAY = 1, PHASE_SUSTAIN = 2, PHASE_RELEASE = 3;
+// From the 10-bit attenuation value over time (averaged and multiplied by 8)
+const ATTENUATION_INCREMENT_TABLE = [
+	0, 0, 4, 4, // 0-15
+	4, 4, 6, 6,
+	4, 5, 6, 7,
+	4, 5, 6, 7,
+
+	4, 5, 6, 7, // 16-31
+	4, 5, 6, 7,
+	4, 5, 6, 7,
+	4, 5, 6, 7,
+
+	4, 5, 6, 7, // 32-47
+	4, 5, 6, 7,
+	4, 5, 6, 7,
+	4, 5, 6, 7,
+
+	8, 10, 12, 14, // 48-63
+	16, 20, 24, 28,
+	32, 40, 48, 56,
+	64, 64, 64, 64
+];
+
 
 function addBuffers(dest, ...sources) {
 	for (let i = 0; i < dest.length; i++) {
@@ -15,18 +39,85 @@ class Operator {
 		this.name = `[${name}]`;
 		this.angle = 0;
 		this.frequency = 0;
-		this.keyOn = false;
-		this.volume = 0;
+		this.amplitude = {
+			counter: 0,
+			phase: PHASE_RELEASE,
+			// for each phase, slope and final point
+			phaseParams: [
+				{ rate: 0, level: 0x3ff }, // PHASE_ATTACK
+				{ rate: 0, level: 0x3ff }, // PHASE_DECAY
+				{ rate: 0, level: 0x3ff }, // PHASE_SUSTAIN
+				{ rate: 0, level: 0x3ff }, // PHASE_RELEASE
+			],
+			attenuation: 0x3ff,
+		};
+	}
+
+	// Done every 3 samples
+	calcAmplitude() {
+		let rate = this.amplitude.phaseParams[this.amplitude.phase].rate;
+		if (rate > 0) {
+			rate *= 2;
+			// TODO Florian -- key scaling
+			if (rate > 63) rate = 63;
+		}
+
+		// Determines every how many iterations we get the attenuation-increment-value in the table
+		let counterShiftValue = 11 - rate >>> 2;
+		if (counterShiftValue < 0) counterShiftValue = 0;
+
+		if (++this.amplitude.counter % (1 << counterShiftValue) === 0) {
+			const attenuationIncrement = ATTENUATION_INCREMENT_TABLE[rate] / 8;
+			if (this.amplitude.phase === PHASE_ATTACK) {
+				// Simulation discrète d'exponentielle
+				this.amplitude.attenuation += (~this.amplitude.attenuation * attenuationIncrement) >> 4;
+			} else {
+				this.amplitude.attenuation += attenuationIncrement;
+			}
+		}
+	}
+
+	processKeyOn(on) {
+		this.amplitude.phase = on ? PHASE_ATTACK : PHASE_RELEASE;
+	}
+
+	processTotalLevelWrite(data) {
+		console.log(`${this.name} total_level=${data}`);
+		this.amplitude.phaseParams[PHASE_ATTACK].level = data;
+		//this.volume = Math.pow(10, -data * 96 / (127 * 20));
+	}
+
+	processRSARWrite(rateScaling, attackRate) {
+		console.log(`${this.name} rate_scaling=${rateScaling}, attack_rate=${attackRate}`);
+		// TODO Florian -- Rate scaling
+		this.amplitude.phaseParams[PHASE_ATTACK].rate = attackRate;
+	}
+
+	processAMDRWrite(amplitudeModulation, decayRate) {
+		console.log(`${this.name} amplitude_mod=${amplitudeModulation}, decay_rate=${decayRate}`);
+		// TODO Florian -- Amplitude modulation
+		this.amplitude.phaseParams[PHASE_DECAY].rate = decayRate;
+	}
+
+	processSustainRateWrite(sustainRate) {
+		console.log(`${this.name} sustain_rate=${sustainRate}`);
+		this.amplitude.phaseParams[PHASE_SUSTAIN].rate = sustainRate;
+	}
+
+	processSLRRWrite(sustainLevel, releaseRate) {
+		console.log(`${this.name} sustain_level=${sustainLevel}, release_rate=${releaseRate * 2 + 1}`);
+		this.amplitude.phaseParams[PHASE_SUSTAIN].level = sustainLevel;
+		this.amplitude.phaseParams[PHASE_RELEASE].rate = releaseRate * 2 + 1;
 	}
 
 	processSamples(samples) {
 		if (this.frequency === 0) return;
 		for (let i = 0; i < samples.length; i++) {
-			const sample = (Math.sin(this.angle) > 0 ? 1 : -1) * this.volume / 24;
-			if (this.keyOn) {
-				this.angle += this.frequency * 2 * Math.PI / SAMPLE_RATE;
-			}
-			samples[i] += sample;
+			//const sample = (Math.sin(this.angle) > 0 ? 1 : -1) * TODO_volume / 24;
+			//if (this.keyOn) {
+			//	this.angle += this.frequency * 2 * Math.PI / SAMPLE_RATE;
+			//}
+			//samples[i] += sample;
 		}
 	}
 }
@@ -91,11 +182,6 @@ class ChannelSet {
 		return this.channels[reg & 3].operators[reg >>> 2 & 3];
 	}
 
-	processAmplitudeWrite(operator, data) {
-		console.log(`${operator.name} amplitude=${data}`);
-		operator.volume = Math.pow(10, -data * 96 / (127 * 20));
-	}
-
 	processFrequencyWrite(channel) {
 		console.log(`${this.channels[channel].name} frequency=${this.channelFrequency(channel)}`);
 		this.channels[channel].operators.forEach(op => op.frequency = this.channelFrequency(channel));
@@ -115,20 +201,41 @@ class ChannelSet {
 
 	processWrite(part, reg, data) {
 		switch (reg & 0xf0) {
-		case 0x40:
-			const operator = this.channelOperator(reg);
-			if (operator) return this.processAmplitudeWrite(operator, data & 0x7f);
-			break;
+			case 0x40: {
+				const operator = this.channelOperator(reg);
+				if (operator) return operator.processTotalLevelWrite(data & 0x7f);
+				break;
+			}
+			case 0x50: {
+				const operator = this.channelOperator(reg);
+				if (operator) return operator.processRSARWrite(data >>> 6, data & 0x1f);
+				break;
+			}
+			case 0x60: {
+				const operator = this.channelOperator(reg);
+				if (operator) return operator.processAMDRWrite(data >>> 7, data & 0x1f);
+				break;
+			}
+			case 0x70: {
+				const operator = this.channelOperator(reg);
+				if (operator) return operator.processSustainRateWrite(data & 0x1f);
+				break;
+			}
+			case 0x80: {
+				const operator = this.channelOperator(reg);
+				if (operator) return operator.processSLRRWrite(data >>> 4, data & 0xf);
+				break;
+			}
 
-		case 0xa0:
-			if (reg < 0xa3) return this.processFrequencyWrite(reg & 3);
-			else if (reg >= 0xa4 && reg < 0xa7) return; // MSB frequency
-			break;
+			case 0xa0:
+				if (reg < 0xa3) return this.processFrequencyWrite(reg & 3);
+				else if (reg >= 0xa4 && reg < 0xa7) return; // MSB frequency
+				break;
 
-		case 0xb0:
-			if (reg < 0xb3) return this.processFeedbackAlgorithm(reg & 3, data);
-			if (reg >= 0xb4 && reg < 0xb7) return this.processStereoLfoSensitivity(reg & 3, data);
-			break;
+			case 0xb0:
+				if (reg < 0xb3) return this.processFeedbackAlgorithm(reg & 3, data);
+				if (reg >= 0xb4 && reg < 0xb7) return this.processStereoLfoSensitivity(reg & 3, data);
+				break;
 		}
 		console.warn(`YM reg=${part}${reg.toHex()} data=${data.toHex()}`);
 	}
@@ -152,7 +259,7 @@ export class YM2612 {
 	processKeyOn(data) {
 		console.log(`Key ${(data & 0x10) ? 'on' : 'off'} channel=${this.channelId(data & 7).name}`);
 		// Bits 4-7 => operators 0-3
-		this.channelId(data & 7).operators.forEach((o, i) => o.keyOn = (data >>> (4 + i)) & 1);
+		this.channelId(data & 7).operators.forEach((o, i) => o.processKeyOn((data >>> (4 + i)) & 1));
 	}
 
 
@@ -163,17 +270,17 @@ export class YM2612 {
 		if (reg < 0x30) {
 			if (part !== 0) return;
 			switch (reg & 0xff) {
-			case 0x27:
-				if (data >>> 6 !== 0) console.error(`Using unsupported channel 3 mode ${data >>> 6}`);
-				return;
+				case 0x27:
+					if (data >>> 6 !== 0) console.error(`Using unsupported channel 3 mode ${data >>> 6}`);
+					return;
 
-			case 0x28:
-				this.processKeyOn(data);
-				return;
+				case 0x28:
+					this.processKeyOn(data);
+					return;
 
-			default:
-				console.warn(`YM reg=${part}${reg.toHex()} data=${data.toHex()}`);
-				return;
+				default:
+					console.warn(`YM reg=${part}${reg.toHex()} data=${data.toHex()}`);
+					return;
 			}
 		}
 
@@ -198,12 +305,12 @@ export class YM2612 {
 		console.log(`Processing frame, active = ${runningOps.join(', ')}`);
 
 		addBuffers(outputSamples,
-			this.channels[0].processSamples(outputSamples.slice()),
-			this.channels[1].processSamples(outputSamples.slice()),
-			this.channels[2].processSamples(outputSamples.slice()),
-			this.channels[3].processSamples(outputSamples.slice()),
-			this.channels[4].processSamples(outputSamples.slice()),
-			this.channels[5].processSamples(outputSamples.slice()),
+				this.channels[0].processSamples(outputSamples.slice()),
+				this.channels[1].processSamples(outputSamples.slice()),
+				this.channels[2].processSamples(outputSamples.slice()),
+				this.channels[3].processSamples(outputSamples.slice()),
+				this.channels[4].processSamples(outputSamples.slice()),
+				this.channels[5].processSamples(outputSamples.slice()),
 		);
 	}
 }
